3807e13fd014e474bbb4ced0ad23880da923c281
joe <>
2024-04-30 16:38:34 -0500

    lazy migration

--- nvim/.config/nvim/lua/dacfg/init.lua
+++ nvim/.config/nvim/lua/dacfg/init.lua
@@ -34,7 +34,6 @@ require('Comment').setup({
   pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
 })
 
-vim.g.mapleader = " "
 
 -- {{{ colorscheme
 --local colorscheme = "gruvbox"
--- nvim/.config/nvim/lua/dacfg/plugins.lua
+++ nvim/.config/nvim/lua/dacfg/plugins.lua
@@ -1,57 +1,59 @@
--- packer bootstrap
-local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+-- lazy.nvim bootstrap
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+
+vim.g.mapleader = " "
+
+local plugins = {}
+
+if vim.env.COPILOT == '1' then
+  table.insert(plugins, 'github/copilot.vim')
 end
 
-local packer_bootstrap = ensure_packer()
-
-return require('packer').startup(function(use)
-  if vim.env.COPILOT == '1' then
-    use 'github/copilot.vim'
-  end
-
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
-
+table.insert(plugins, {
   -- Configurations for Nvim LSP
-  use 'neovim/nvim-lspconfig'
-  use 'williamboman/mason.nvim'
-  use 'williamboman/mason-lspconfig.nvim'
-  use 'williamboman/nvim-lsp-installer'
+  'neovim/nvim-lspconfig',
+  'williamboman/mason.nvim',
+  'williamboman/mason-lspconfig.nvim',
+  'williamboman/nvim-lsp-installer',
 
   -- lua config
-  use 'folke/neodev.nvim'
+  'folke/neodev.nvim',
 
   -- lsp status
-  use { 'j-hui/fidget.nvim', opts = {} }
+  { 'j-hui/fidget.nvim', opts = {} },
 
   -- null-ls
-  use {
+  {
     'jose-elias-alvarez/null-ls.nvim',
-    requires = "nvim-lua/plenary.nvim",
-  }
-  use 'jose-elias-alvarez/typescript.nvim'
+    dependencies = "nvim-lua/plenary.nvim",
+  },
+  'jose-elias-alvarez/typescript.nvim',
 
   -- treesitter
-  use {
+  {
     'nvim-treesitter/nvim-treesitter',
-    requires = {
+    dependencies = {
       'nvim-treesitter/nvim-treesitter-textobjects',
     },
-    run = ':TSUpdate'
-  }
+    build = ':TSUpdate'
+  },
 
   -- completion
-  use {
+  {
     'hrsh7th/nvim-cmp',
-    requires = {
+    dependencies = {
       -- Autocompletion
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-path',
@@ -60,17 +62,17 @@ return require('packer').startup(function(use)
       'L3MON4D3/LuaSnip',
       'rafamadriz/friendly-snippets',
     },
-  }
+  },
 
   -- markdown preview
-  use({
+  {
     "iamcco/markdown-preview.nvim",
-    run = function() vim.fn["mkdp#util#install"]() end,
-  })
+    build = function() vim.fn["mkdp#util#install"]() end,
+  },
 
   -- movement
-  use 'takac/vim-hardtime'
-  use {
+  'takac/vim-hardtime',
+  {
     'jinh0/eyeliner.nvim',
     config = function()
       require 'eyeliner'.setup {
@@ -78,99 +80,97 @@ return require('packer').startup(function(use)
         dim = true
       }
     end
-  }
-  use {
+  },
+  {
     'ThePrimeagen/harpoon',
-    requires = 'nvim-lua/plenary.nvim',
-  }
+    dependencies = 'nvim-lua/plenary.nvim',
+  },
 
   -- lint
-  use 'mfussenegger/nvim-lint'
+  'mfussenegger/nvim-lint',
 
   --caddyfile
-  use 'isobit/vim-caddyfile'
+  'isobit/vim-caddyfile',
 
   -- find/replace
-  use { 'nvim-pack/nvim-spectre',
-    requires = 'nvim-lua/plenary.nvim',
-  }
+  { 'nvim-pack/nvim-spectre',
+    dependencies = 'nvim-lua/plenary.nvim',
+  },
 
   --grepper
-  use 'mhinz/vim-grepper'
+  'mhinz/vim-grepper',
 
   -- fzf
-  use { 'ibhagwan/fzf-lua',
-    requires = { 'kyazdani42/nvim-web-devicons' } -- optional for icon support
-  }
+  { 'ibhagwan/fzf-lua',
+    dependencies = { 'kyazdani42/nvim-web-devicons' } -- optional for icon support
+  },
 
   -- debugging
-  use 'mfussenegger/nvim-dap'
-  use 'leoluz/nvim-dap-go'
+  'mfussenegger/nvim-dap',
+  'leoluz/nvim-dap-go',
 
   -- lf
-  use 'VebbNix/lf-vim'
-  use 'ptzz/lf.vim'
-  use 'voldikss/vim-floaterm'
+  'VebbNix/lf-vim',
+  'ptzz/lf.vim',
+  'voldikss/vim-floaterm',
 
   -- git
-  use 'airblade/vim-gitgutter'
+  'airblade/vim-gitgutter',
   -- open in github
-  use {
+  {
     'ruifm/gitlinker.nvim',
-    requires = 'nvim-lua/plenary.nvim',
-  }
-  use { 'akinsho/git-conflict.nvim', tag = "*", config = function()
-    require('git-conflict').setup()
-  end }
+    dependencies = 'nvim-lua/plenary.nvim',
+  },
+
+  { 'akinsho/git-conflict.nvim', version = "*", config = true},
 
   --editorconfig
-  use 'editorconfig/editorconfig-vim'
+  'editorconfig/editorconfig-vim',
 
   -- auto-commenting
-  use 'numToStr/Comment.nvim'
-  use 'JoosepAlviste/nvim-ts-context-commentstring'
+  'numToStr/Comment.nvim',
+  'JoosepAlviste/nvim-ts-context-commentstring',
 
   -- surround
-  use 'kylechui/nvim-surround'
+  'kylechui/nvim-surround',
   -- auto close brackets
-  use 'rstacruz/vim-closer'
+  'rstacruz/vim-closer',
   -- auto end functions
-  use 'tpope/vim-endwise'
+  'tpope/vim-endwise',
   -- auto close html tags
-  use 'windwp/nvim-ts-autotag'
+  'windwp/nvim-ts-autotag',
 
 
   -- go
-  use 'ray-x/go.nvim'
-  use 'ray-x/guihua.lua' -- recommended if need floating window support
+  'ray-x/go.nvim',
+  'ray-x/guihua.lua', -- recommended if need floating window support
 
   -- java
-  use 'mfussenegger/nvim-jdtls'
+  'mfussenegger/nvim-jdtls',
 
   -- nvim-test
-  use { "klen/nvim-test" }
+   "klen/nvim-test",
 
   -- lualine
-  use {
+  {
     'nvim-lualine/lualine.nvim',
-    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-  }
+    dependencies = { 'kyazdani42/nvim-web-devicons', opt = true }
+  },
 
-  use { 'f-person/git-blame.nvim',
-    requires = { 'f-person/lua-timeago' }
-  }
+  { 'f-person/git-blame.nvim',
+    dependencies = { 'f-person/lua-timeago' }
+  },
 
   -- tf syntax
-  use 'hashivim/vim-terraform'
+  'hashivim/vim-terraform',
   -- just syntax
-  use 'NoahTheDuke/vim-just'
+  'NoahTheDuke/vim-just',
 
   -- Color scheme
-  use 'dikiaap/minimalist'
-  use 'morhetz/gruvbox'
-  use 'folke/tokyonight.nvim'
+  'dikiaap/minimalist',
+  'morhetz/gruvbox',
+  'folke/tokyonight.nvim',
 
-  if packer_bootstrap then
-    require('packer').sync()
-  end
-end)
+})
+
+require('lazy').setup(plugins)
